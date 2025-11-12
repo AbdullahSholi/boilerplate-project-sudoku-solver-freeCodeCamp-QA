047f4520736c3b2b4b26fd0cd22b26a6b23394f1
AbdullahSholi <groupgroup060@gmail.com>
2025-11-12 18:52:08 +0200

    --empty

--- server.js
+++ server.js
@@ -39,7 +39,9 @@ app.use(function(req, res, next) {
 const PORT = process.env.PORT || 3000
 app.listen(PORT, function () {
   console.log("Listening on port " + PORT);
-  // process.env.NODE_ENV='test'
+  if (process.env.NODE_ENV !== 'test' && process.env.RUN_FCC_TESTS === 'true') {
+    process.env.NODE_ENV = 'test';
+  }
   if (process.env.NODE_ENV==='test') {
     console.log('Running Tests...');
     setTimeout(function () {
